0be400dda58e2154bbe0cb817e09cd8fdd509d90
hzoou <woojooo52@naver.com>
2019-10-16 15:04:40 +0900

    Install cors module
    client 에서의 접근을 허용하기 위해 cors 모듈 설치

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
