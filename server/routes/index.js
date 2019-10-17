e077d004b31c2bb9f0823dda21a803b51bf2e749
hzoou <woojooo52@naver.com>
2019-10-18 03:20:50 +0900

    Create reservation router
    예약을 담당하는 router 생성

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,14 @@ const router = express.Router();
 const loginRouter = require('./login');
 const logoutRouter = require('./logout');
 const roomRouter = require('./room');
+const reservationRouter = require('./reservation');
+
+const { checkToken } = require('../middlewares/auth');
 
 router.use('/login', loginRouter);
 router.get('/logout', logoutRouter);
 router.use('/rooms', roomRouter);
+router.use(checkToken);
+router.use('/reservation', reservationRouter);
 
 module.exports = router;
