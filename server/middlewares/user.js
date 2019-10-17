7b31fb8f4b5738a7261455e0b658408f8547e399
hzoou <woojooo52@naver.com>
2019-10-18 03:55:35 +0900

    Modify jwt token expiresIn
    10 분에서 7 일로 변경

--- server/middlewares/user.js
+++ server/middlewares/user.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const publishToken = (req, res) => {
     const payload = { id: req.user.id, name: req.user.name, email: req.user.email };
-    const expiresIn = { expiresIn: '10m' };
+    const expiresIn = { expiresIn: '7d' };
     const token = jwt.sign(payload, process.env.JWT_SECRET, expiresIn);
     res.cookie("jwt", token, { httpOnly: true }).send({ user: req.user, token: token });
 };
