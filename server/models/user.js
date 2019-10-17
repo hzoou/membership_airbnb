a8e27994cb98be482616585bd830e0b6dc5aaae5
hzoou <woojooo52@naver.com>
2019-10-17 14:41:06 +0900

    Modify user model
    email coulmn 추가

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const USER = sequelize.define('USER', {
-    google_id: { type: DataTypes.STRING(150), unique: true },
-    name: { type: DataTypes.STRING(40) },
-    image: { type: DataTypes.TEXT },
+    google_id: { type: DataTypes.STRING(150), unique: true, allowNull: false },
+    email: { type: DataTypes.STRING(80), unique: true, allowNull: false },
+    name: { type: DataTypes.STRING(40), allowNull: false },
+    image: { type: DataTypes.TEXT, allowNull: false },
   }, {
     timestamps: false,
     freezeTableName: true
