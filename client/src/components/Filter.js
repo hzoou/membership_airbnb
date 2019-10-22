887e9b1d33c5360626ab01e6baa9c0918cf763b6
hzoou <woojooo52@naver.com>
2019-10-22 22:48:07 +0900

    Modify Filter UI
    스크롤 여부와 관계없이 필터의 위치가 상단에 고정되도록 수정

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,37 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
+import DateFilter from "./filter/DateFilter";
+import GuestFilter from "./filter/GuestFilter";
+import TypeFilter from "./filter/TypeFilter";
+import PriceFilter from "./filter/PriceFilter";
+import EtcFilter from "./filter/EtcFilter";
+
+const FilterContainer = styled.div`
+    height: 51px;
+`;
 
 const FilterDiv = styled.div`
     height: 50px;
+    width: 100%;
     display: flex;
+    position: fixed;
+    top: 86px;
+    z-index: 10;
     padding: 0 72px;
+    background: white;
     border-bottom: 1px solid #e4e4e4;
 `;
 
-const FilterBar = styled.button`
-    height: 30px;
-    padding: 0 10px;
+const FilterBar = styled.div`
     margin: auto 0 auto 10px;
-    background: white;
-    border: 1px solid #F2F2F2;
-    border-radius: 3px;
-    cursor: pointer;
-    outline: none;
-    
-    &:hover {
-        background: #F2F2F2;
-    }  
 `;
 
 const Filter = () => {
     return (
-        <FilterDiv>
-            <FilterBar>날짜</FilterBar>
-            <FilterBar>인원</FilterBar>
-            <FilterBar>숙소 유형</FilterBar>
-            <FilterBar>가격</FilterBar>
-            <FilterBar>필터 추가하기</FilterBar>
-        </FilterDiv>
+        <FilterContainer>
+            <FilterDiv>
+                <FilterBar>
+                    <DateFilter />
+                </FilterBar>
+                <FilterBar>
+                    <GuestFilter />
+                </FilterBar>
+                <FilterBar>
+                    <TypeFilter />
+                </FilterBar>
+                <FilterBar>
+                    <PriceFilter />
+                </FilterBar>
+                <FilterBar>
+                    <EtcFilter />
+                </FilterBar>
+            </FilterDiv>
+        </FilterContainer>
     )
 };
 
