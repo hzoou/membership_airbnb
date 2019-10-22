a915e0d62be04d8c8f6447ae861e19c13da72b13
hzoou <woojooo52@naver.com>
2019-10-22 22:51:09 +0900

    Remove unused file
    사용하지 않는 css 파일 제거

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from "./components/Header";
 import Filter from "./components/Filter";
 import Container from "./components/Container";
-import './App.css';
 
 const App = () => {
   return (
