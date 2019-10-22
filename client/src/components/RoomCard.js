3a558fc4353e39abe991c4a76c43606c10650ab8
hzoou <woojooo52@naver.com>
2019-10-22 22:49:43 +0900

    Rename datas to rooms
    state로 관리할 숙소 정보를 datas 에서 rooms 로 변경

--- client/src/components/RoomCard.js
+++ client/src/components/RoomCard.js
@@ -70,7 +70,7 @@ const Star = styled.svg`
 `;
 
 const RoomCard = () => {
-    const [ datas, setData ] = useState([]);
+    const [ rooms, setRoomData ] = useState([]);
 
     const getAllRooms = () => {
         fetch('http://localhost:8080/api/rooms', {
@@ -80,7 +80,7 @@ const RoomCard = () => {
             if (res.ok) return res.json();
             throw new Error('Network response was not ok.');
         }).then((result) => {
-            if (result.status === 'SUCCESS') return setData(result.data);
+            if (result.status === 'SUCCESS') return setRoomData(result.data);
             new Error(result);
         }).catch((err) => {
             return alert(err.message);
@@ -89,24 +89,24 @@ const RoomCard = () => {
 
     useEffect(getAllRooms, []);
 
-    const isLoading = (datas == null);
+    const isLoading = (rooms == null);
 
-    return ( isLoading ? <div>Loading...</div>  : <div>{datas.map((data) => {
-        return <Card key={data.id}>
+    return ( isLoading ? <div>Loading...</div>  : <div>{rooms.map((room) => {
+        return <Card key={room.id}>
                     <div>
-                        <Thumbnail src={data.thumbnail} />
+                        <Thumbnail src={room.thumbnail} />
                     </div>
                     <CardContent>
-                        <Type>{data.type}</Type>
-                        <Title>{data.title}</Title>
-                        <Option>인원 {data.guest}명 ・ 침대 {data.bed}개 ・ 침실 {data.bedroom}개 ・ 욕실 {data.bathroom}개</Option>
-                        <Price>₩{commaToNumber(data.price)}/박</Price>
+                        <Type>{room.type}</Type>
+                        <Title>{room.title}</Title>
+                        <Option>인원 {room.guest}명 ・ 침대 {room.bed}개 ・ 침실 {room.bedroom}개 ・ 욕실 {room.bathroom}개</Option>
+                        <Price>₩{commaToNumber(room.price)}/박</Price>
                         <StarDiv>
                             <Star viewBox="0 0 576 512">
                                 <path fill="#398288"
                                       d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
                             </Star>
-                            {data.star}</StarDiv>
+                            {room.star}</StarDiv>
                     </CardContent>
                 </Card>
     })}</div> )
