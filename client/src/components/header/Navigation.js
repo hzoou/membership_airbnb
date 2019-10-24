827de7565b8c4ffa771929b59291bf2a5c714b84
hzoou <woojooo52@naver.com>
2019-10-24 10:36:44 +0900

    Modify navigation component
    로그인 되었을 때, 사용자의 구글 프로필 이미지가 로그인 버튼 대신 보여짐
    프로필 이미지 눌렀을 때, confirm 창으로 확인 받고, 쿠키를 삭제해서 로그아웃 시킴

--- client/src/components/header/Navigation.js
+++ client/src/components/header/Navigation.js
@@ -1,33 +1,71 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 
-const Navigation = styled.div`
-    height: 80px;
-    display: flex;
-    align-items: center;
-    margin-left: auto;
-    margin-right: 20px;
-    font-size: 14px;
-    font-weight: 600;
-`;
-
-const NavItem = styled.div`
-    height: -webkit-fill-available;
-    line-height: 80px;
-    margin-right: 25px;
-    
-    &:hover {
-        border-bottom: 3px solid #b8b8b8;
-        cursor: pointer;
-    }
-`;
-
-const Login = styled.a`
-    color: #484848;
-    text-decoration: none;
-`;
-
 export default () => {
+    const [ user, setUser ] = useState(null);
+
+    const Navigation = styled.div`
+        height: 80px;
+        display: flex;
+        align-items: center;
+        margin-left: auto;
+        margin-right: 20px;
+        font-size: 14px;
+        font-weight: 600;
+    `;
+
+    const NavItem = styled.div`
+        height: -webkit-fill-available;
+        line-height: 80px;
+        margin-right: 25px;
+        
+        &:hover {
+            border-bottom: 3px solid #b8b8b8; 
+            cursor: pointer;
+        }
+    `;
+
+    const Login = styled.a`
+        color: #484848;
+        text-decoration: none;
+    `;
+
+    const Profile = styled.img`
+        width: 35px;
+        border-radius: 50%;
+        cursor: pointer;
+    `;
+
+    const getJwtToken = async () => {
+        const cookies = document.cookie.split(';');
+        for (const cookie of cookies) {
+            const [ key, value ] = cookie.trim().split('=');
+            if (key === 'jwt') {
+                try {
+                    const res = await fetch('http://localhost:8080/api/user', { method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({ token: value }) });
+                    const result = await res.json();
+                    setUser(result.data);
+                } catch (e) {
+                    console.log(e.message);
+                }
+            }
+        }
+    };
+
+    const clickProfile = () => {
+        const confirm = window.confirm('로그아웃 하시겠습니까?');
+        if (!confirm) return;
+        deleteCookie('jwt');
+
+    };
+
+    const deleteCookie = (key) => {
+        document.cookie = key + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;';
+        window.location.reload();
+    };
+
+    useEffect(() => { getJwtToken() }, []);
+
     return (
         <Navigation>
             <NavItem>
@@ -36,8 +74,11 @@ export default () => {
             <NavItem>
                 <span>도움말</span>
             </NavItem>
+    {user ? <Profile src="https://lh3.googleusercontent.com/a-/AAuE7mD0kzR2EMXnwtDB3EO9riv9ilRbWvufCrXg4eTk" onClick={clickProfile}/> :
             <NavItem>
                 <span><Login href="http://localhost:8080/api/login">로그인</Login></span>
+            </NavItem>}
+            <NavItem>
             </NavItem>
         </Navigation>
     )
