4581ed0f7f50db46469c66e9640869188d11aba2
hzoou <woojooo52@naver.com>
2019-10-24 10:30:35 +0900

    Add function to send user info
    sendUserData : 사용자 정보 반환

--- server/middlewares/user.js
+++ server/middlewares/user.js
@@ -1,12 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 const publishToken = (req, res) => {
-    const payload = { id: req.user.id, name: req.user.name, email: req.user.email };
+    const payload = { id: req.user.id, name: req.user.name, image: req.user.image };
     const expiresIn = { expiresIn: '7d' };
     const token = jwt.sign(payload, process.env.JWT_SECRET, expiresIn);
-    res.cookie("jwt", token, { httpOnly: true }).redirect('http://localhost:3000');
+    res.cookie("jwt", token).redirect('http://localhost:3000');
+};
+
+const sendUserData = (req, res) => {
+    res.send({ status: 'SUCCESS', message: '유효한 토큰입니다.', data: req.user });
 };
 
 module.exports = {
-    publishToken
+    publishToken,
+    sendUserData
 };
