522e6ecc83a94b6674c8ab7724095d3767e2302c
hzoou <woojooo52@naver.com>
2019-10-25 10:34:58 +0900

    Modify dateFilter component

--- client/src/components/filter/popup/DateFilter.js
+++ client/src/components/filter/popup/DateFilter.js
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DayPickerRangeController } from "react-dates";
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import '../../../style/dateFilter.css';
 
-export default () => {
-    const [ startDate, setStartDate ] = useState();
-    const [ endDate, setEndDate ] = useState();
+export default ({ state, setState }) => {
+    const [ startDate, setStartDate ] = useState(state.startDate);
+    const [ endDate, setEndDate ] = useState(state.endDate);
     const [ focusedInput, setFocusedInput ] = useState('startDate');
 
     const onDatesChange = ({ startDate, endDate }) => {
         setStartDate(startDate);
         setEndDate(endDate);
+        if (startDate && endDate) setState({ startDate, endDate });
+        else if (startDate) setState({ startDate, endDate: null });
+        else if (endDate) setState({ startDate: null, endDate });
     };
 
     const onFocusChange = focusedInput => {
-        if (!focusedInput) setFocusedInput('startDate');
-        else setFocusedInput(focusedInput);
+        setFocusedInput(!focusedInput ? 'startDate' : focusedInput);
     };
+
+    const initState = () => {
+        setStartDate(state.startDate);
+        setEndDate(state.endDate);
+    };
+
+    useEffect(initState, [ state ]);
 
     return (
         <DayPickerRangeController
@@ -27,6 +36,7 @@ export default () => {
             onFocusChange={onFocusChange}
             onDatesChange={onDatesChange}
             hideKeyboardShortcutsPanel={true}
+            isOutsideRange={day => day.isSameOrBefore(new Date().getTime() - (24 * 60 * 60 * 1000))}
             numberOfMonths={2}
         />
     );
